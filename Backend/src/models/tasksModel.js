bdfba39678624c2056e63e0dc6627517855bb7aa
Kaio Soledade <soledadekaio@gmail.com>
2024-01-30 11:33:00 -0300

    connecção entre tasksController e tasksModel

--- Backend/src/models/tasksModel.js
+++ Backend/src/models/tasksModel.js
@@ -1,6 +1,12 @@
-const getAll = () => {
+const connection = require('./connection');
 
+const getAll = async () => {
+    const tasks = await connection.execute(~'SELECT * FROM tasks');
+    return tasks;
 };
 module.exports = {
     getAll
 };
+
+// (async) significa assíncrona
+// (await) significa esperar concluir para poder executar
--- Backend/src/router.js
+++ Backend/src/router.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
 const router = express.Router();
 
+const tasksController = require('./controllers/tasksController')
 
 
 
-// router.get('/', (request, response) => response.status(200).send('Router esta funcionando'));
-//               (res   , res     )
+
+router.get('/tasks',  tasksController.getAll);
+
+// (request, response)
+// (res   , res     )
+// temos uma rota(Router) do tipo GET no ednpoint "/tasks", e ela vai executar uma Controller.getAll . Então retornara todas sua tasks para a gente
 
 module.exports = router;
